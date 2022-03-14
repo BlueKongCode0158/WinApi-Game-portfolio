0454047fd5ce9f55eb03b31f4ad17a1e0a5091ae
BlueKongCode0158 <bluekcodeplant6587@gmail.com>
2022-03-15 00:03:06 +0900

    32bit

--- Demonstration_Test/stdafx.h
+++ Demonstration_Test/stdafx.h
@@ -28,7 +28,7 @@ using namespace std;
 #include <algorithm>
 #include <assert.h>
 
-#pragma comment(lib, "fmodex_vc.lib");
+#pragma comment(lib, "fmodex_vc.lib")
 
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
